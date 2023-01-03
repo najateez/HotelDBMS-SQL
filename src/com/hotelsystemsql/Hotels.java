8bfce029ca7ad44ba330f9cb791a117bfded6b68
Najat <Najat>
2023-01-03 21:08:02 +0400

    insert values in some tables + read inserted values depend on user want and print it on consol

--- src/com/hotelsystemsql/Hotels.java
+++ src/com/hotelsystemsql/Hotels.java
@@ -1,17 +1,60 @@
 package com.hotelsystemsql;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Random;
+import java.util.Scanner;
+
+import com.mysql.jdbc.PreparedStatement;
 
 
 
 public class Hotels {
 	
-	public static void readFromTable(){
-	
+	public static void readFromHotelsTable(){
 		
+		    String url = "jdbc:mysql://localhost:3306/hoteldbms";
+		    String user = "root";
+	        String pass = "10@104Ar$";
+	    
+			Scanner in = new Scanner(System.in);
+			
+			int readRows;
+			System.out.println("Enter how many users you want to print on consol:");
+			readRows = in.nextInt();
+			
+			int count = 0;
+			Connection con = null;
+			
+			try {
+				 Driver driver = (Driver) Class.forName("com.mysql.jdbc.Driver").newInstance();
+				 DriverManager.registerDriver(driver);
+				 con = DriverManager.getConnection(url, user, pass);
+				 Statement st = con.createStatement();
+				 
+				 //as mr explaination on board
+				 String sql = "SELECT * FROM Hotels";
+				 ResultSet rs = st.executeQuery(sql);  //ResultSet class import from library
+				 
+				while (rs.next() && count < readRows) {
+					int id = rs.getInt("id");
+					String hotel_name = rs.getString("hotel_name");
+					String hotel_locationn = rs.getString("hotel_location");
+					Date created_date = rs.getDate("created_date");
+					Date updated_date = rs.getDate("updated_date");
+					boolean is_Active = rs.getBoolean("is_Active");
+					
+					System.out.println(id + " " + hotel_name + hotel_locationn +" " + created_date + " " + updated_date + " " + is_Active + " ");
+					count++;
+				 }
+				con.close();
+			}catch (Exception ex) {
+				System.err.println(ex);
+			}
 		}
 		
 	public static void getById(){
@@ -30,11 +73,57 @@ public class Hotels {
 		
 		}
 
-	public void insertIntoTable(){
+	public static void insertIntoHotelsTable(){
+		
+		 String url = "jdbc:mysql://localhost:3306/hoteldbms";
+		 String user = "root";
+	     String pass = "10@104Ar$";
+	     
+	     Connection con = null;
+	     PreparedStatement prst = null;
+	     
+	     Scanner in= new Scanner(System.in);
+	     
+	     System.out.println("Enter how many rows you want to insert:");
+	     int noOfRowsToBeInserted=in.nextInt();
+	     
+	   /*      String hotel_name="bustan";
+	   	     String hotel_location="Athaiba"; */
+	   	     Date created_date = new Date(System.currentTimeMillis()); //will take current date/today's date.
+	   	     Date updated_date= new Date(System.currentTimeMillis());
+	   //	     int is_Active=1;
+	   	     
+	   	  try {  //register driver and connect with sql
+     	        Driver driver = (Driver) Class.forName("com.mysql.jdbc.Driver").newInstance();
+	            DriverManager.registerDriver(driver);
+	            
+	            con = DriverManager.getConnection(url, user,pass);
+	            Statement st = con.createStatement();
+	            
+	             
+               for (int i = 0; i < noOfRowsToBeInserted; i++) {
+               Random rn = new Random();   //here i import library of class Random.
+               int numberToAdd = rn.nextInt(100);
+               int id=i+numberToAdd;
+               
+               String hotel_name = "najat" + numberToAdd;
+               String hotel_location = "najat" + numberToAdd;
+               int is_Active = 1;  
+                
+               String sql = "INSERT INTO Hotels VALUES ("+id+",'"+hotel_name+"','"+hotel_location+"','"+created_date+"','"+updated_date+"',"+is_Active+")";
+               
+	            prst = (PreparedStatement) con.prepareStatement(sql);
+	            prst.executeUpdate();
 
-		}
+               System.out.println(sql);
+             }
+               
+       } catch (Exception e) {
+           System.out.println(e.getMessage());
+       }
+   }
 	
-	  // the way of creating table in database using java code
+	 // the way of creating table in database using java code
     public static boolean isHotelsTableCreated() {
 		
 
@@ -78,6 +167,8 @@ public class Hotels {
 	public static void main(String[] args) {
 		
 		isHotelsTableCreated();
+		readFromHotelsTable();
+		insertIntoHotelsTable();
 
 	}
 
--- src/com/hotelsystemsql/Room_Type.java
+++ src/com/hotelsystemsql/Room_Type.java
@@ -1,16 +1,58 @@
 package com.hotelsystemsql;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Random;
+import java.util.Scanner;
+
+import com.mysql.jdbc.PreparedStatement;
 
 public class Room_Type {
 	
-	public static void readFromTable(){
+	public static void readFromTableRoomType(){	
 		
-		
-	}
+		    String url = "jdbc:mysql://localhost:3306/hoteldbms";
+		    String user = "root";
+	        String pass = "10@104Ar$";
+	    
+			Scanner in = new Scanner(System.in);
+			
+			int readRows;
+			System.out.println("Enter how many users you want to print on consol:");
+			readRows = in.nextInt();
+			
+			int count = 0;
+			Connection con = null;
+			
+			try {
+				 Driver driver = (Driver) Class.forName("com.mysql.jdbc.Driver").newInstance();
+				 DriverManager.registerDriver(driver);
+				 con = DriverManager.getConnection(url, user, pass);
+				 Statement st = con.createStatement();
+				 
+				 //follow mr explaination on board
+				 String sql = "SELECT * FROM Room_Type";
+				 ResultSet rs = st.executeQuery(sql);  //ResultSet class import from library
+				 
+				while (rs.next() && count < readRows) {
+					int roomType_id = rs.getInt("roomType_id");
+					String room_type_name = rs.getString("room_type_name");
+					Date created_date = rs.getDate("created_date");
+					Date updated_date = rs.getDate("updated_date");
+					boolean is_Active = rs.getBoolean("is_Active");
+					
+					System.out.println(roomType_id + " " + room_type_name + " " + created_date + " " + updated_date + " " + is_Active + " ");
+					count++;
+				 }
+				con.close();
+			}catch (Exception ex) {
+				System.err.println(ex);
+			}
+		}
 	
 public static void getById(){
 	
@@ -28,9 +70,56 @@ public static void makeIsActiveFalseById() {
 	
 	}
 
-public void insertIntoTable(){
+public static void insertIntoRoomTypeTable(){
+	
+	String url = "jdbc:mysql://localhost:3306/hoteldbms";
+	 String user = "root";
+    String pass = "10@104Ar$";
+    
+    Connection con = null;
+    PreparedStatement prst = null;
+    
+    Scanner in= new Scanner(System.in);
+    
+    System.out.println("Enter how many rows you want to insert:");
+    int noOfRowsToBeInserted=in.nextInt();
+    
+   //      String room_type_name="TripleRoom";
+  	     Date created_date = new Date(System.currentTimeMillis());
+  	     Date updated_date= new Date(System.currentTimeMillis());
+  //	     int is_Active=1;
+  	     
+  	  try {  //register driver and connect with sql
+	       Driver driver = (Driver) Class.forName("com.mysql.jdbc.Driver").newInstance();
+           DriverManager.registerDriver(driver);
+           
+           con = DriverManager.getConnection(url, user,pass);
+           Statement st = con.createStatement();
+           
+            
+          for (int i = 0; i < noOfRowsToBeInserted; i++) {
+          Random rn = new Random();   //here i import library of class Random.
+          int numberToAdd = rn.nextInt(100);
+          int roomType_id=i+numberToAdd;
+          
+          String room_type_name = "najat" + numberToAdd;
+          int is_Active = 1;  
+          
+          
+          String roomTypeSql = "INSERT INTO Room_Type VALUES ("+roomType_id+",'"+room_type_name+"','"+created_date+"','"+updated_date+"',"+is_Active+")";
+          
+           prst = (PreparedStatement) con.prepareStatement(roomTypeSql);
+           prst.executeUpdate();
 
-	}
+          System.out.println(roomTypeSql);
+        }
+          
+  } catch (Exception e) {
+      System.out.println(e.getMessage());
+  }
+}
+
+	
 	
 	// the way of creating table in database using java code
     public static boolean isRoomTypeTableCreated() {
@@ -76,6 +165,8 @@ public void insertIntoTable(){
 	public static void main(String[] args) {
 		
 		isRoomTypeTableCreated();
+		readFromTableRoomType();
+		insertIntoRoomTypeTable();
 
 	}
 
